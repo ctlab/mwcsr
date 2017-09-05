2a1ec17f0beb0c7808f4d51a64622ba5e7be8eb0
Alexander Loboda <loboda@rain.ifmo.ru>
2017-09-05 17:55:31 +0300

    tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(mwcsr)
+
+test_check("mwcsr")
---
+++ tests/testthat/test_rmwcs.R
@@ -0,0 +1,25 @@
+context("rmwcs solver")
+
+data("GAM")
+set.seed(20170905L)
+
+test_that("rmwcs solver doesn't crash on simple graphs", {
+    solve <- rmwcs()
+    size <- 10
+
+    test_graph <- function(make) {
+        scores <- runif(size) - 0.5;
+        solve(make(size) %>% set.vertex.attribute("score", value = scores))
+    }
+    test_graph(make_ring)
+    test_graph(make_star)
+    test_graph(make_full_graph)
+})
+
+test_that("rmwcs solver works fine on GAM instances", {
+    solve <- rmwcs()
+    for (instance in GAM) {
+        sol <- solve(instance)
+        expect_true(is.connected(sol))
+    }
+})
