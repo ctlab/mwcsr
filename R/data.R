435768148c88a475624f6aa52c296ec96f247257
Alexander Loboda <aleks.loboda@gmail.com>
2024-07-16 23:50:56 +0300

    Minor fix: devtools::document()

--- R/data.R
+++ R/data.R
@@ -1,8 +1,8 @@
 #' GAM instance for MWCS problem
 #'
 #' A dataset containing some real-world instances appeared in network
-#' enrichment analysis tool
-#' \doi{10.1093/nar/gkw266}{Shiny GAM}
+#' enrichment analysis tool Shiny GAM
+#' (\doi{10.1093/nar/gkw266}).
 #'
 #' @format A vector of named vertex-weighted igraph instances
 #' @source \url{http://dimacs11.zib.de/instances/MWCS-GAM.zip}
